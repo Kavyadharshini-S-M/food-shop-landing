e84e700a04c797bc3b6a1ed5e542cc44579f17ad
Kavyadharshini <smkavyadharshini@gmail.com>
2026-03-25 15:08:16 +0530

    Add custom colors for food shop theme

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8B4513',
+        secondary: '#D2691E',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
